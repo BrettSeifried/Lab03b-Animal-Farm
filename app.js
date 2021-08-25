392e100eac0bbc7568b8875349dc2d586141c5b2
Brett Seifried <seifriedbrett@gmail.com>
2021-08-25 15:37:38 -0700

    create page with animals and audio

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // import functions
 
 // reference needed DOM elements
+const catImage = document.getElementById('cat-image');
+const catAudio = document.getElementById('cat-audio');
+const dogImage = document.getElementById('dog-image');
+const dogAudio = document.getElementById('dog-audio');
+const horseImage = document.getElementById('horse-image');
+const horseAudio = document.getElementById('horse-audio');
+
+console.log(catImage, catAudio);
+
+catImage.addEventListener('click', () => {
+  catAudio.play();
+});
+
+dogImage.addEventListener('click', () => {
+  dogAudio.play();
+});
+
+horseImage.addEventListener('click', () => {
+  horseAudio.play();
+});
 
 
 // set event listeners 
